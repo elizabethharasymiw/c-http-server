2136bf19ca37ae28c3f083dec545de7f2e046f86
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-05-27 21:50:10 -0500

    Add networking headers and comments

--- http-server.c
+++ http-server.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <stdio.h>      // c standard input output
+#include <stdlib.h>     // c standard lib
+#include <string.h>     // c string lib
+
+#include <sys/socket.h> // socket-related functions and structures
+#include <netinet/in.h> // IPv4 and IPv6 structures
+#include <apra/inet.h>  // functions for manipulating numeric IP addresses and port numbers
+
+#include <unistd.h>     // provides access to the POSIX operating system API
+
+void handle_request(int client_socket);
 
 int main(int argc, char* argv[]){
     printf("Hello, World!\n");
+
+    int server_socket, client_socket;
+
     return 0;
 
 }
