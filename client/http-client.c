0131b1a4bcae8fc8633bd5391babd118308c5150
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-05-27 23:41:32 -0500

    Create folders for client verse server code

---
+++ client/http-client.c
@@ -0,0 +1,16 @@
+#include <stdio.h>      // c standard input output
+#include <stdlib.h>     // c standard lib
+#include <string.h>     // c string lib
+
+#include <sys/socket.h> // socket-related functions and structures
+#include <netinet/in.h> // IPv4 and IPv6 structures
+//#include <apra/inet.h>  // functions for manipulating numeric IP addresses and port numbers
+
+#include <unistd.h>     // provides access to the POSIX operating system API
+
+
+int main(int argc, char* argv[]){
+    printf("Hello, World!\n");
+    return 0;
+
+}
